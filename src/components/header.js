f041f125d42e892758ece359bd65f0ad8359dd32
Devon Martin <devmart10@gmail.com>
2020-04-21 16:09:32 -0700

    cleans up header component

--- src/components/header.js
+++ src/components/header.js
@@ -3,23 +3,20 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex mb-4 text-gray-100 bg-indigo-700">
-      <div className="flex items-baseline p-2 my-container">
+    <div className="mb-4 text-lg text-gray-100 bg-indigo-700">
+      <div className="flex flex-wrap items-center p-2 my-container">
         <Link href="/">
-          <a className="text-xl font-bold">Devon Martin</a>
+          <a className="mr-auto font-bold">Devon Martin</a>
         </Link>
-        <button className="w-10 h-10 ml-auto border rounded md:hidden">
-          <span className="block w-6 h-6 mx-auto border rounded"></span>
-        </button>
-        <ul className="items-end hidden ml-auto text-lg md:flex">
+        <ul className="flex">
           <Link href="/projects">
-            <a className="ml-2">Projects</a>
+            <a className="">Projects</a>
           </Link>
           <Link href="/about">
-            <a className="ml-2">About</a>
+            <a className="ml-4">About</a>
           </Link>
           <Link href="/contact">
-            <a className="ml-2">Contact</a>
+            <a className="ml-4">Contact</a>
           </Link>
         </ul>
       </div>
