7b5403d661d98e5523ba7c963de65bdee7f914e0
Devon Martin <devmart10@gmail.com>
2020-04-27 23:28:03 -0700

    removes broken links and dropdown

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { CSSTransition } from "react-transition-group";
 import { FaBars } from "./Icons";
+import { FaGlasses } from "react-icons/fa";
 
 const Links = ({ className }) => (
   <ul className={`flex items-center h-10 font-light tracking-wide ${className}`}>
@@ -22,21 +23,21 @@ const Header = () => {
   return (
     <header className="mb-4 text-lg text-gray-100 shadow-lg bg-primary">
       <div className="p-2 my-container">
-        <div className="flex items-center justify-between">
-          <button
+        <div className="flex items-center">
+          {/* <button
             className="flex justify-center w-8 h-8 p-1 mr-2 border rounded sm:hidden"
             onClick={() => setDropdown((state) => !state)}
           >
             <FaBars className="fill-current"></FaBars>
-          </button>
-          <Link href="/">
-            <a className="mr-auto text-2xl font-semibold">Devon Martin</a>
-          </Link>
-          <Links className="hidden sm:flex"></Links>
+          </button> */}
+          {/* <FaGlasses className="w-10 h-10 p-2 mr-2 bg-white rounded-full fill-current text-primary"></FaGlasses> */}
+          <a className="mr-2 text-2xl font-semibold tracking-wider">Devon Martin</a>
+
+          {/* <Links className="hidden sm:flex"></Links> */}
         </div>
-        <CSSTransition in={dropdown} timeout={300} classNames="header-dropdown" unmountOnExit>
+        {/* <CSSTransition in={dropdown} timeout={300} classNames="header-dropdown" unmountOnExit>
           <Links></Links>
-        </CSSTransition>
+        </CSSTransition> */}
       </div>
     </header>
   );
