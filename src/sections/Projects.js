9be97f7c0f7966b9bcd440b5369eb0757cc4c86e
Devon Martin <devmart10@gmail.com>
2020-05-01 13:24:44 -0700

    add project content

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -6,7 +6,7 @@ const Projects = () => {
     <section id="projects">
       <div className="mb-4 bg-white card">
         <h2 className="font-bold text-primary">Projects</h2>
-        {data.map(({ name, date, shortDesc, longDesc }, index) => {
+        {data.map(({ name, date, shortDesc, longDesc, tech_stack }, index) => {
           return (
             <div className="mt-4" key={index}>
               <div className="flex flex-col md:flex-row md:items-baseline ">
@@ -15,6 +15,11 @@ const Projects = () => {
                 <span className="pb-2 text-sm text-gray-700 md:ml-auto">{date}</span>
               </div>
               <p>{longDesc}</p>
+              <div className="flex pb-8">
+                {tech_stack.map(item => (
+                  <span className="px-2 mr-2 text-sm text-gray-600 bg-gray-300 rounded">{item}</span>
+                ))}
+              </div>
             </div>
           );
         })}
