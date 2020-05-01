8f6a39d8a4753e563032fc804568996220d95919
Devon Martin <devmart10@gmail.com>
2020-05-01 14:37:34 -0700

    rework socials component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
+import Socials from "../components/Socials";
+import { FaEnvelope } from "react-icons/fa";
+
+import data from "../data/personal.json";
 
 const Header = () => {
   return (
     <header className="mb-4 shadow-lg bg-primary">
-      <div className="flex items-center p-2 my-container">
-        <h3 className="mb-0 font-semibold tracking-wider text-gray-100">Devon Martin</h3>
+      <div className="flex flex-wrap items-baseline p-2 my-container">
+        <h2 className="mb-0 mr-auto font-semibold tracking-wider text-gray-100">Devon Martin</h2>
+        <div className="flex text-xl text-gray-100">
+          <Socials></Socials>
+          <a href={`mailto:${data.email}`} className="font-normal">
+            <FaEnvelope></FaEnvelope>
+          </a>
+        </div>
       </div>
     </header>
   );
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -5,17 +5,17 @@ import data from "../data/personal.json";
 
 const Socials = () => {
   return (
-    <div className="flex text-2xl">
-      <a target="_blank" rel="noopener" href={data.socials.github} className="pr-2 font-normal">
+    <div className="flex">
+      <a target="_blank" rel="noopener" href={data.socials.github} className="pr-2">
         <FaGithub></FaGithub>
       </a>
-      <a target="_blank" rel="noopener" href={data.socials.facebook} className="pr-2 font-normal">
+      <a target="_blank" rel="noopener" href={data.socials.facebook} className="pr-2">
         <FaFacebook></FaFacebook>
       </a>
-      <a target="_blank" rel="noopener" href={data.socials.twitter} className="pr-2 font-normal">
+      <a target="_blank" rel="noopener" href={data.socials.twitter} className="pr-2">
         <FaTwitter></FaTwitter>
       </a>
-      <a target="_blank" rel="noopener" href={data.socials.linkedin} className="pr-2 font-normal">
+      <a target="_blank" rel="noopener" href={data.socials.linkedin} className="pr-2">
         <FaLinkedin></FaLinkedin>
       </a>
     </div>
