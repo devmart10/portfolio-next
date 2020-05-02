bd3e7898bcc40d5ca359f0b91bdcbe7804953e8b
Devon Martin <devmart10@gmail.com>
2020-05-02 11:42:18 -0700

    adds education section

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,14 @@ import AboutMe from "../src/sections/AboutMe";
 import Projects from "../src/sections/Projects";
 import Contact from "../src/sections/Contact";
 import Work from "../src/sections/Work";
+import Education from "../src/sections/Education";
 
 const Index = () => {
   return (
     <Layout>
       <AboutMe></AboutMe>
       <Work></Work>
+      <Education></Education>
       <Projects></Projects>
       <Contact></Contact>
     </Layout>
